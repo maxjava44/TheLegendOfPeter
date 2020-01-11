4585aacc9c278067a6ac5d3f060d813cde6ca2b2
maximumdeveloping <max.gilsoul@live.com>
2020-01-11 12:49:16 +0100

    Update Main.java
    Ist momentan erstmal nur ein leeres Fenster aber wird ja noch

--- src/main/java/group/thelegendofpeter/Main.java
+++ src/main/java/group/thelegendofpeter/Main.java
@@ -6,17 +6,76 @@
 
 package group.thelegendofpeter;
 
-/**
- *
- * @author max
- */
-public class Main {
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+//Klasse um das Fenster des Spiels einzurichten und die Spiellogik/grafik zu implementieren
+public class Main extends Canvas implements Runnable{
+    private static final long serialVersionUID = 1L;
+    boolean running; //Gibt an ob das Spiel läuft oder nicht
+    public static final int Width = 160; //Die Breite des Fensters
+    public static final int Height = Width / 12 *9; //Die Höhe des Fensters
+    public static final int Scale = 3;//Der Größenskalierungsfaktor mit dem das Fenster vergrößert wird
+    public static final String name = "TheLegendOfPeter";//Der Name des Fensters
+    private JFrame frame; //Das Fenster
+    
+    public Main() //Konstruktor der Klasse
+    {
+    	setMinimumSize(new Dimension(Width*Scale, Height*Scale));
+    	setMaximumSize(new Dimension(Width*Scale, Height*Scale));
+    	setPreferredSize(new Dimension(Width*Scale, Height*Scale));
+        frame = new JFrame(name); //Gibt dem Fenster den Namen name
+        frame.setMinimumSize(new Dimension(Width*Scale, Height*Scale));//Setzen die Größe des Fensters
+    	frame.setMaximumSize(new Dimension(Width*Scale, Height*Scale));
+    	frame.setPreferredSize(new Dimension(Width*Scale, Height*Scale));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Wenn Fenster geschlossen wird schließst sich auch das Programm
+        frame.setLayout(new BorderLayout()); //Setzt ein Borderlayout damit für die obere Fensterleiste Platz ist
+        frame.add(this,BorderLayout.CENTER);//Setzt das Canvas(der Spielanzeigebereich) in die Mitte 
+        frame.setResizable(false);//Fenster kann nicht vergrößert/verkleinert werden
+        frame.setLocationRelativeTo(null);//Fenster wird mittig platziert
+        frame.setVisible(true);//Fenster wird sichtbar
+    }
+    public synchronized void start() //Startet einen Thread in dem das Spiel läuft
+    {
+    	running = true;
+    	new Thread(this).start();
     }
     
+    public synchronized void stop() //Stoppt das Spiel
+    {
+    	running = false;
+    }
+    
+    public void run() //ruft die Spielelogik 60 mal in der Sekunde auf
+    {
+    	long lastTime = System.nanoTime();
+    	double nsPerTick = 1000000000D/60D;
+    	int frames = 0;
+    	int ticks = 0;
+    	long lastTimer = System.currentTimeMillis();
+    	double delta = 0;
+    	while(running)
+    	{
+    		long now = System.nanoTime();
+    		delta += (now - lastTime);
+    		tick();
+    		render();
+    	}
+    }
+    
+    public void tick() //Aktualiesert die Spiellogik
+    {
+    	
+    }
+    
+    public void render() //Aktualiesiert den Spieleanzeigebereich
+    {
+    	
+    }
+    public static void main(String args[]) //Startet das Spiel
+    {
+    	new Main().start();
+    }
 }
