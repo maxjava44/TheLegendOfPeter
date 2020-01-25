b78aa83b51ed7cece788e66a46ded2971941cf01
maximumdeveloping <max.gilsoul@live.com>
2020-01-25 15:24:24 +0100

    Update SpriteSheet.java

--- src/main/java/group/thelegendofpeter/SpriteSheet.java
+++ src/main/java/group/thelegendofpeter/SpriteSheet.java
@@ -9,29 +9,19 @@ import javax.imageio.ImageIO;
 public class SpriteSheet {
 	private BufferedImage image;
 	private ArrayList<Sprite> sprites = new ArrayList<Sprite>();
-	private String path;
-	private int rows;
-	private int cols;
-	private int width;
-	private int height;
-	public SpriteSheet(String pPath,int pRows,int pCols,int pWidth,int pHeight)
+	public SpriteSheet(String path,int rows,int cols,int width,int height)
 	{
             
 		try {
-			image = ImageIO.read(this.getClass().getClassLoader().getResource(pPath));
+			image = ImageIO.read(this.getClass().getClassLoader().getResource(path));
 		} catch (IOException e) {
 			System.out.println("Lesen des SpriteSheets unmöglich");
 		}
-		rows = pRows;
-		cols = pCols;
-		width = pWidth;
-		height = pHeight;
-		int[][] positions = new int[rows*cols][4]; 
 		for(int i = 0;i<cols;i++)
 		{
 			for(int s = 0;s<rows;s++)
 			{
-				sprites.add(new Sprite(positions[i*s][0],positions[i*s][1],positions[i*s][2],positions[i*s][3],image.getSubimage(width*s, height*i, width, height).getRGB(0,0,width,height,null,0,width)));
+				sprites.add(new Sprite(0,0,image.getSubimage(width*s, height*i, width, height).getRGB(0,0,width,height,null,0,width)));
 			}
 		}
 	}
