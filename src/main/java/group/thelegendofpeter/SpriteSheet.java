83c505fdf9a55215e028a66ded8381c86b923e9e
maximumdeveloping <max.gilsoul@live.com>
2020-01-25 15:40:41 +0100

    Update SpriteSheet.java

--- src/main/java/group/thelegendofpeter/SpriteSheet.java
+++ src/main/java/group/thelegendofpeter/SpriteSheet.java
@@ -7,22 +7,21 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class SpriteSheet {
-	private BufferedImage image;
 	private ArrayList<Sprite> sprites = new ArrayList<Sprite>();
 	public SpriteSheet(String path,int rows,int cols,int width,int height)
 	{
             
 		try {
-			image = ImageIO.read(this.getClass().getClassLoader().getResource(path));
+			BufferedImage image = ImageIO.read(this.getClass().getClassLoader().getResource(path));
+			for(int i = 0;i<cols;i++)
+			{
+				for(int s = 0;s<rows;s++)
+				{
+					sprites.add(new Sprite(0,0,image.getSubimage(width*s, height*i, width, height).getRGB(0,0,width,height,null,0,width)));
+				}
+			}
 		} catch (IOException e) {
 			System.out.println("Lesen des SpriteSheets unmöglich");
-		}
-		for(int i = 0;i<cols;i++)
-		{
-			for(int s = 0;s<rows;s++)
-			{
-				sprites.add(new Sprite(0,0,image.getSubimage(width*s, height*i, width, height).getRGB(0,0,width,height,null,0,width)));
-			}
 		}
 	}
 	
