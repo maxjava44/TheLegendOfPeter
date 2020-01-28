d25508fe22bdfb08a1888ebaaa8a726441bbbc34
maximumdeveloping <max.gilsoul@live.com>
2020-01-28 19:16:58 +0100

    Update Screen.java

--- src/main/java/group/thelegendofpeter/Screen.java
+++ src/main/java/group/thelegendofpeter/Screen.java
@@ -1,49 +1,11 @@
 package group.thelegendofpeter;
 
-import java.awt.event.KeyEvent;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.util.ArrayList;
 
-public class Screen implements java.awt.event.KeyListener {
+public class Screen {
 
-    private String path;
-    private SpriteSheet sheet;
     private int[][] pixel2d = new int[Main.Width][Main.Height];
-    public ArrayList<Sprite> sprites = new ArrayList<Sprite>();
-    public Screen(String pPath,SpriteSheet pSheet)
-    {
-        path = pPath;
-        sheet = pSheet;
-    }
-    
-    public void loadLevel(int id)
-    {
-    	sprites.clear();
-        try{
-            BufferedReader levelfile = new BufferedReader(new FileReader(new File(this.getClass().getClassLoader().getResource(path + id).toURI())));
-            int[] infos = new int[3];
-            int counter = 0;
-            String content;
-            while((content = levelfile.readLine())!= null)
-            {
-            	infos[counter] = Integer.parseInt(content);
-            	counter++;
-            	if(counter == 3)
-            	{
-            		Sprite sprite = new Sprite(infos[1],infos[2],sheet.getSpriteList().get(infos[0]).getPixel());
-            		sprites.add(sprite);
-            		counter = 0;
-            	}
-            }
-            levelfile.close();
-        }catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
+    ArrayList<Sprite> sprites;
     public void assemble()
     {
     	for(int x = 0;x<Main.Width;x++)
@@ -67,41 +29,14 @@ public class Screen implements java.awt.event.KeyListener {
     	}
     }
     
+    public void setSprites(ArrayList<Sprite> pSprites)
+    {
+    	sprites = pSprites;
+    }
+    
     public int[][] getPixel()
     {
     	return pixel2d;
     }
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		if(!(sprites.isEmpty()))
-		{
-			switch(e.getKeyChar())
-			{
-			case 'w':sprites.get(0).setY(sprites.get(0).getY()-10);
-			break;
-			case 's':sprites.get(0).setY(sprites.get(0).getY()+10);
-			break;
-			case 'a':sprites.get(0).setX(sprites.get(0).getX()-10);
-			break;
-			case 'd':sprites.get(0).setX(sprites.get(0).getX()+10);
-			break;
-			}
-		}
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-    
 }
 
