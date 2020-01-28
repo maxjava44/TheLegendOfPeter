f0ae7e55c9e1587e5b7da4ccce8d464e36da5dba
maximumdeveloping <max.gilsoul@live.com>
2020-01-28 19:13:09 +0100

    Update Character.java

--- src/main/java/group/thelegendofpeter/Character.java
+++ src/main/java/group/thelegendofpeter/Character.java
@@ -1,5 +1,22 @@
 package group.thelegendofpeter;
 
 public class Character {
-
+	private Sprite sprite;
+	private int speed;
+	
+	public Character(Sprite pSprite,int pSpeed)
+	{
+		sprite = pSprite;
+		speed = pSpeed;
+	}
+	
+	public int getSpeed()
+	{
+		return speed;
+	}
+	
+	public Sprite getSprite()
+	{
+		return sprite;
+	}
 }
