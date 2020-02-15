ed4802d07665c5417caf9d8502d8bc9ed3447ebd
maximumdeveloping <max.gilsoul@live.com>
2020-02-15 22:37:42 +0100

    Update Screen.java

--- src/main/java/group/thelegendofpeter/Screen.java
+++ src/main/java/group/thelegendofpeter/Screen.java
@@ -1,21 +1,57 @@
 package group.thelegendofpeter;
 
+import java.awt.Color;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
+
+import javax.imageio.ImageIO;
 
 public class Screen {
 
     private int[][] pixel2d = new int[Main.Width][Main.Height];
     ArrayList<Sprite> sprites;
+    Sprite background;
+    public Screen(){
+    	try {
+			background = new Sprite(0,0,ImageIO.read(this.getClass().getClassLoader().getResource("background.png")).getRGB(0,0,64,64,null,0,64));
+		} catch (IOException e) {
+			int[] backgroundalt = new int[64*64];
+			for(int i = 0;i<backgroundalt.length;i++)
+			{
+				backgroundalt[i] = java.awt.Color.RED.getRGB();
+			}
+			background = new Sprite(0,0,backgroundalt);
+		}
+    }
+    
     public void assemble()
     {
     	for(int x = 0;x<Main.Width;x++)
         {
             for(int y = 0;y<Main.Height;y++)
             {
-            	pixel2d[x][y] = java.awt.Color.BLACK.getRGB();
+            	pixel2d[x][y] = new Color(0,0,0,255).getRGB();
             }
         }
+    	int xHelp = 0;
+    	int yHelp = 0;
+    	for(int i = 0;i<12*12;i++)
+    	{
+    		for(int x = xHelp;x<64+xHelp;x++)
+    		{
+                for(int y = yHelp;y<64+yHelp;y++)
+                {
+                	pixel2d[y][x] = background.getPixel()[(y-yHelp) * 64 + (x-xHelp)];
+                }
+            }
+    		xHelp = xHelp + 64;
+    		if(xHelp > 704)
+    		{
+    			xHelp = 0;
+    			yHelp = yHelp + 64;
+    		}
+    	}
     	for(Sprite sprite : sprites)
     	{
     		int yOff = sprite.getY();
