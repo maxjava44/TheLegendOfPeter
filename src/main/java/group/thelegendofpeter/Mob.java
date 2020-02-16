8c3fcb5b5357a79a0ab6744ff12341c9d6c692a2
PeterB <59706391+PeterB2020@users.noreply.github.com>
2020-02-16 16:39:41 +0100

    Update Mob.java
    Das kommt aus einem anderen Projekt und idk ob die Idee gut ist

--- src/main/java/group/thelegendofpeter/Mob.java
+++ src/main/java/group/thelegendofpeter/Mob.java
@@ -7,3 +7,32 @@ public class Mob extends Character {
 		super(pSprite,pSpeed);
 	}
 }
+
+/*
+Was von diesem Code soll man dann überhaupt übernehmen?
+Ist es nicht leichter die HP hier an den Mob zu binden?
+
+public class Mob extends Character {
+	
+	public Mob(Sprite pSprite,int pSpeed)
+	{
+		super(pSprite,pSpeed);
+	}
+	}
+
+
+    private int health;
+    public Mob(int health) {
+        this.health = health;
+    }
+
+    public void hit(int damage) {
+        health -= damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+    
+    
+    */
