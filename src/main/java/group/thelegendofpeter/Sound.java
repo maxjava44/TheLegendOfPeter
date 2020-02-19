7c34091e0bf84555edbb96dfda0dc96f9c62aa1f
maximumdeveloping <max.gilsoul@live.com>
2020-02-19 18:10:01 +0100

    Update Sound.java

--- src/main/java/group/thelegendofpeter/Sound.java
+++ src/main/java/group/thelegendofpeter/Sound.java
@@ -5,27 +5,13 @@
  */
 package group.thelegendofpeter;
 
-/**
- *
- * @author Maximilian Pitzke
- */
-import java.io.File;
-import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.InputStream;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-/**
- *
- * @author Tristan Michels
- */
+
 class Sound {
-
-
- 
-    
-
+	
      Clip clip;
    
     public Sound(InputStream stream)
@@ -33,7 +19,6 @@ class Sound {
         try
         {
             AudioInputStream eingabe = AudioSystem.getAudioInputStream(stream);
-           
             clip = AudioSystem.getClip();
             clip.open(eingabe);
         }
@@ -42,17 +27,13 @@ class Sound {
    
     public void start()
     {
-       
         clip.start();
-     
     }
     
     public void start_LOOP()
     {
-       
         clip.start();
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
-        //JOptionPane.showMessageDialog(,"ss");        
+        clip.loop(Clip.LOOP_CONTINUOUSLY);        
     }
     
      public void stop()
@@ -65,26 +46,10 @@ class Sound {
         return clip.isRunning();
     }
    
-      public void Sound_Start()
+    public void Sound_Start()
     {
-       Sound test; 
-       test = new Sound(this.getClass().getClassLoader().getResourceAsStream("sound/Purple Planet Music - Space Journey (1_20).wav"));
-       test.start_LOOP();
+    	 Sound test; 
+    	 test = new Sound(this.getClass().getClassLoader().getResourceAsStream("sound/Purple Planet Music - Space Journey (1_20).wav"));
+    	 test.start_LOOP();
     }
-       public void Sound_Start2()
-    {
-       
-              
-                  //Sound test = new Sound("C:/A_Netbeans/TheLegendOfPeter-master/Lauf.wav"); 
-                   // test.start();
-                    
-              
-           }
-           
-  public void Sound_Stoppen()
-    {
-       
-       stop();
-    }
-
-    }
+}
