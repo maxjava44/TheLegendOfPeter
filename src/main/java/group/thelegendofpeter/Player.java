de93a36915fec4168a5468562c24f45071d9f86b
tristanjava <59748262+tristanjava@users.noreply.github.com>
2020-02-26 08:29:26 +0100

    Update Player.java

--- src/main/java/group/thelegendofpeter/Player.java
+++ src/main/java/group/thelegendofpeter/Player.java
@@ -2,8 +2,8 @@ package group.thelegendofpeter;
 
 public class Player extends Character {
 		
-	public Player(Sprite pSprite,int pSpeed)
+	public Player(Sprite pSprite,int pSpeed,int pHealth,int pMaxHealth,int pAttackDamage, boolean pDead)
 	{
-		super(pSprite,pSpeed);
+		super(pSprite,pSpeed,int pHealth,int pMaxHealth,int pAttackDamage, boolean pDead);
 	}
 }
