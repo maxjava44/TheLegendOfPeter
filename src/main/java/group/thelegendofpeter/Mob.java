0b9545ca500736f2178c25ee53a191a045356630
maximumdeveloping <max.gilsoul@live.com>
2020-02-26 20:38:17 +0100

    Update Mob.java

--- src/main/java/group/thelegendofpeter/Mob.java
+++ src/main/java/group/thelegendofpeter/Mob.java
@@ -2,45 +2,12 @@ package group.thelegendofpeter;
 
 public class Mob extends Character {
 	
-	public Mob(Sprite pSprite,int pSpeed)
+	public Mob(Sprite pSprite,int pSpeed,int pHealth,int pMaxHealth,boolean pDead)
 	{
-		super(pSprite,pSpeed);
+		super(pSprite,pSpeed, pSpeed, pHealth, pMaxHealth, pDead);
 	}
 	
 	private int health = 50;
 
 	
 }
-
-/*
-Was von diesem Code soll man dann überhaupt übernehmen?
-Ist es nicht leichter die HP hier an den Mob zu binden?
-
-public class Mob extends Character {
-	
-	public Mob(Sprite pSprite,int pSpeed)
-	{
-		super(pSprite,pSpeed);
-	}
-	}
-
-	xPosition = (int)("Zahl" * Math.random());
-        yPosition = (int)("Zahl * Math.random());	
-	
-	
-    private int health;
-    public Mob(int health) {
-        this.health = health;
-    }
-
-    public void hit(int damage) {
-        health -= damage;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-    
-    
-    
-    */
