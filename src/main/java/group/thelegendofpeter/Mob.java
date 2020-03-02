b5965ac96c7b57de31cd79face81835f687e6272
tristanjava <59748262+tristanjava@users.noreply.github.com>
2020-03-02 16:28:50 +0100

    Update Mob.java

--- src/main/java/group/thelegendofpeter/Mob.java
+++ src/main/java/group/thelegendofpeter/Mob.java
@@ -2,12 +2,18 @@ package group.thelegendofpeter;
 
 public class Mob extends Character {
 	
-	public Mob(Sprite pSprite,int pSpeed,int pHealth,int pMaxHealth,int pAttackDamage,boolean pDead)
+	public Mob(Sprite pSprite,int pSpeed,int pHealth,int pAttackDamage,boolean pDead)
 	{
-		super(pSprite,pSpeed, pHealth, pMaxHealth,pAttackDamage, pDead);
+		super(pSprite,pSpeed,pHealth,pAttackDamage,pDead);
 	}
 	
-	private int health = 50;
-
+	public double getDistance()
+	{
+		return distance;
+	}
 	
+	public void setDistance (double pdistance)
+	{
+		distance = pdistance;
+	}
 }
