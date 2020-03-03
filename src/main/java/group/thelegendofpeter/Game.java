a8d1813ac5fb53dd01508faa9745f773f6d2b5f2
maximumdeveloping <max.gilsoul@live.com>
2020-03-03 17:34:05 +0100

    Update Game.java

--- src/main/java/group/thelegendofpeter/Game.java
+++ src/main/java/group/thelegendofpeter/Game.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import javax.imageio.ImageIO;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Game implements KeyListener {
