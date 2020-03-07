48113871de58dcedadcfb5b009e4ebaa6dd885cc
maximumdeveloping <max.gilsoul@live.com>
2020-03-07 16:40:02 +0100

    Update Mob.java

--- src/main/java/group/thelegendofpeter/Mob.java
+++ src/main/java/group/thelegendofpeter/Mob.java
@@ -1,21 +1,27 @@
 package group.thelegendofpeter;
 
 public class Mob extends Character {
-	
+
 	private double distance;
-	
-	public Mob(Sprite pSprite,int pSpeed,int pHealth,int pAttackDamage,boolean pDead)
-	{
-		super(pSprite,pSpeed,pHealth,pAttackDamage,pDead);
+
+	/**
+	 * Erstellt ein Mob-Objekt mit den gegebenen Daten
+	 * 
+	 * @param pSprite       Der Sprite des Mobs
+	 * @param pSpeed        Die Geschwindigkeit des Mobs
+	 * @param pHealth       Die Lebenspunkte des Mobs
+	 * @param pAttackDamage Der Angriffsschaden des Mobs
+	 * @param pDead         Legt fest ob der Mob tod ist
+	 */
+	public Mob(Sprite pSprite, int pSpeed, int pHealth, int pAttackDamage, boolean pDead) {
+		super(pSprite, pSpeed, pHealth, pAttackDamage, pDead);
 	}
-	
-	public double getDistance()
-	{
+
+	public double getDistance() {
 		return distance;
 	}
-	
-	public void setDistance (double pdistance)
-	{
+
+	public void setDistance(double pdistance) {
 		distance = pdistance;
 	}
 }
