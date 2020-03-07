cf630a8f1e8777313cfd7b5166f0560c97a16caf
maximumdeveloping <max.gilsoul@live.com>
2020-03-07 16:37:25 +0100

    Update Player.java

--- src/main/java/group/thelegendofpeter/Player.java
+++ src/main/java/group/thelegendofpeter/Player.java
@@ -1,9 +1,16 @@
 package group.thelegendofpeter;
 
 public class Player extends Character {
-		
-	public Player(Sprite pSprite,int pSpeed,int pHealth,int pAttackDamage, boolean pDead)
-	{
-		super(pSprite,pSpeed,pHealth,pAttackDamage,pDead);
+	/**
+	 * Erstellt ein Player-Objekt mit den gegebenen Daten
+	 * 
+	 * @param pSprite       Der Sprite des Players
+	 * @param pSpeed        Die Geschwindigkeit des Players
+	 * @param pHealth       Die Lebenspunkte des Players
+	 * @param pAttackDamage Der Angriffsschaden des Players
+	 * @param pDead         Legt fest ob der Player tod ist
+	 */
+	public Player(Sprite pSprite, int pSpeed, int pHealth, int pAttackDamage, boolean pDead) {
+		super(pSprite, pSpeed, pHealth, pAttackDamage, pDead);
 	}
 }
