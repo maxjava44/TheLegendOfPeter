0e2f45ee2ef21b2d2493144406e99b0c4ed7cfc6
maximumdeveloping <max.gilsoul@live.com>
2020-03-08 14:20:42 +0100

    Update Sound.java

--- src/main/java/group/thelegendofpeter/Sound.java
+++ src/main/java/group/thelegendofpeter/Sound.java
@@ -6,39 +6,34 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
+/**
+ * Verwaltet den Sound in einen extra Thread
+ * 
+ * @author Maximilian Gilsoul,Maximilian Pitzke,Tristan Michels
+ *
+ */
 class Sound extends Thread {
-	
-     Clip clip;
-     boolean playing = false;
-   
-    public Sound(String name,InputStream stream)
-    {
-        super(name);
-        try
-        {
-            AudioInputStream eingabe = AudioSystem.getAudioInputStream(new BufferedInputStream(stream));
-            clip = AudioSystem.getClip();
-            clip.open(eingabe);
-        }
-        catch (Exception e) { e.printStackTrace(); }
-    }
- 
-   
-    public boolean isRunning()
-    {
-        return clip.isRunning();
-    }
 
+	Clip clip;
+	boolean playing = false;
+
+	public Sound(String name, InputStream stream) {
+		super(name);
+		try {
+			AudioInputStream eingabe = AudioSystem.getAudioInputStream(new BufferedInputStream(stream));
+			clip = AudioSystem.getClip();
+			clip.open(eingabe);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void run() {
-		while(true)
-		{
-			if(playing)
-			{
+		while (true) {
+			if (playing) {
 				clip.loop(Clip.LOOP_CONTINUOUSLY);
-			}
-			else
-			{
+			} else {
 				clip.stop();
 				clip.setMicrosecondPosition(0);
 			}
